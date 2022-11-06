5a2ce2cde639e2cec716d75b911d1d94d485a1e6
MushfiqMoon <mushfiq.moon013@google.com>
2022-11-07 05:16:20 +0600

    category data api created

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function dbConnect() {
 dbConnect();
 
 const questionCollection = client.db('sjQuizDB').collection('questions')
+const categoryCollection = client.db('sjQuizDB').collection('category')
 
 // endpoint
 
@@ -34,6 +35,7 @@ app.get('/', (req, res) => {
     res.send('welcome')
 })
 
+//  Questions 
 app.post('/quiz/question', async (req, res) => {
     try {
 
@@ -64,6 +66,54 @@ app.get('/quiz/question', async (req, res) => {
     try {
 
         const cursor = questionCollection.find({})
+        const result = await cursor.toArray()
+        console.log(result)
+        res.send({
+            sussess: true,
+            massage: "Question Created",
+            data: result
+        })
+
+    }
+    catch (error) {
+        res.send({
+            sussess: false,
+            error: "Not created"
+        })
+    }
+})
+
+// Category 
+app.post('/quiz/category', async (req, res) => {
+    try {
+
+        const result = await categoryCollection.insertOne(req.body)
+        console.log(result)
+        if (result.insertedId) {
+            res.send({
+                sussess: true,
+                massage: "Category Created"
+            })
+        }
+        else {
+            res.send({
+                sussess: false,
+                error: "Not created"
+            })
+        }
+    }
+    catch (error) {
+        res.send({
+            sussess: false,
+            error: "Not created"
+        })
+    }
+})
+
+app.get('/quiz/category', async (req, res) => {
+    try {
+
+        const cursor = categoryCollection.find({})
         const result = await cursor.toArray()
         console.log(result)
         res.send({
